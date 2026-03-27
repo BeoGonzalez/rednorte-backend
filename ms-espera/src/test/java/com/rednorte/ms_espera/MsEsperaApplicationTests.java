53251f6f500362ef4c579edcfefdc51fc9d96276
BeoGonzalez <beo.gonzalez@duocuc.cl>
2026-03-27 01:07:56 -0300

    Se agregó la api gateway, eureka server y el ms de lista de espera

---
+++ ms-espera/src/test/java/com/rednorte/ms_espera/MsEsperaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rednorte.ms_espera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEsperaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
