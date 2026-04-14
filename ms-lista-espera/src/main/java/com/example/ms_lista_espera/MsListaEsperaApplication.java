75f499a33971583dc10ab5fbae88132f7be51b7b
BeoGonzalez <beo.gonzalez@duocuc.cl>
2026-04-14 10:19:51 -0400

    Se agregó el docker compose de lista-espera y el nuevo ms

---
+++ ms-lista-espera/src/main/java/com/example/ms_lista_espera/MsListaEsperaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_lista_espera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsListaEsperaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsListaEsperaApplication.class, args);
+	}
+
+}
