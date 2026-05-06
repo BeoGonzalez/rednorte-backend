b927af91d55a9df81867ae26b4ddabfbe887d793
BeoGonzalez <beo.gonzalez@duocuc.cl>
2026-05-06 12:51:56 -0400

    El openFeign está listo

--- bff/src/main/java/com/example/bff/client/PacienteClient.java
+++ bff/src/main/java/com/example/bff/client/PacienteClient.java
@@ -3,15 +3,14 @@ package com.example.bff.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
-// El "name" DEBE coincidir con el nombre que ms-pacientes usa en Eureka
 @FeignClient(name = "ms-pacientes")
 public interface PacienteClient {
 
-    // Copiamos la misma firma del endpoint que existe en ms-pacientes
     @GetMapping("/api/pacientes/{id}")
-    Object obtenerPacientePorId(@PathVariable("id") Long id);
-
-    // NOTA: Usamos "Object" por rapidez, pero lo ideal es que crees un DTO
-    // en el BFF que represente lo que quieres recibir.
+    Object obtenerPacientePorId(
+            @PathVariable("id") Long id,
+            @RequestHeader("Authorization") String token // ⬅️ El BFF ahora enviará el token
+    );
 }
--- bff/src/main/java/com/example/bff/controller/BffDashboardController.java
+++ bff/src/main/java/com/example/bff/controller/BffDashboardController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bff/api/v1/dashboard")
-@CrossOrigin(origins = "http://localhost:4200") // Permite la conexión desde Angular
+@CrossOrigin(origins = "http://localhost:4200")
 public class BffDashboardController {
 
     private final PacienteClient pacienteClient;
@@ -18,12 +18,12 @@ public class BffDashboardController {
     }
 
     @GetMapping("/paciente-resumen/{id}")
-    public ResponseEntity<Object> obtenerResumenPaciente(@PathVariable Long id) {
-        // El BFF llama al ms-pacientes a través de Feign
-        Object datosPaciente = pacienteClient.obtenerPacientePorId(id);
+    public ResponseEntity<Object> obtenerResumenPaciente(
+            @PathVariable Long id,
+            @RequestHeader("Authorization") String token) { // ⬅️ 1. Atrapamos el token de Angular
 
-        // Aquí podrías agregar más llamadas (ej. a ms-lista-espera)
-        // y mezclar los datos antes de devolverlos.
+        // 2. Le pasamos el token al cliente Feign
+        Object datosPaciente = pacienteClient.obtenerPacientePorId(id, token);
 
         return ResponseEntity.ok(datosPaciente);
     }
