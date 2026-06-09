85751453cd4627f440025cc90c15d27ed1dfa26a
BeoGonzalez <beo.gonzalez@udocuc.cl>
2026-06-09 12:27:21 -0400

    Se creó el chatbot ara consulta de sintomas

---
+++ ms-chatbot/src/main/java/com/example/ms_chatbot/MsChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsChatbotApplication.class, args);
+	}
+
+}
